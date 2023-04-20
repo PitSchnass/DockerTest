f940579fd836b27ffdc6c027c533d1a1e1e85677
henning <henning@laptop.com>
2023-04-20 23:05:05 +0200

    added TestFixture.cs

---
+++ TestFixture.cs
@@ -0,0 +1,16 @@
+namespace DockerTest;
+
+[TestFixture]
+public class TestFixture
+{
+    [OneTimeSetUp]
+    public void Initialize()
+    {
+        
+    }
+    
+    [OneTimeTearDown]
+    public void Dispose()
+    {
+    }
+}
